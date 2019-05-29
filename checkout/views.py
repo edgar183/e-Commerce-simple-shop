3753a119e92801973339a4824307d168d7c7e1a4
Edgars Peskaitis <edgars.peskaitis@gmail.com>
2019-05-29 12:56:48 +0000

    typo

--- checkout/views.py
+++ checkout/views.py
@@ -53,4 +53,4 @@ def checkout(request):
         payment_form = MakePaymentForm()
         order_form = OrderForm()
     
-    return render(request, "checkout.html", {'order_form': order_form, 'payment_form': payment_form, 'publishanle': settings.STRIPE_PUBLISHABLE})
+    return render(request, "checkout.html", {'order_form': order_form, 'payment_form': payment_form, 'publishable': settings.STRIPE_PUBLISHABLE})
